88bafd7896cdd14f2ed4b7ee4bcd69c86e26655e
Mike Thompson <mike.thompson@day8.com.au>
2016-07-16 15:51:09 +1000

    Add tests

--- test/re_frame_async_flow_fx/core_test.cljs
+++ test/re_frame_async_flow_fx/core_test.cljs
@@ -35,3 +35,50 @@
          [(nth rules 1)]))))
 
 
+(deftest test-massage-rules
+  (is (= (core/massage-rules :my-id [{:when :seen? :events :1 :dispatch [:2]}])
+         (list {:id 0 :when core/all-events-seen? :events #{:1} :dispatch (list [:2])})))
+
+  (is (= (core/massage-rules :my-id [{:when :seen-both? :events [:1 :2] :dispatch :halt}])
+         (list {:id 0 :when core/all-events-seen? :events #{:1 :2} :dispatch (list [:my-id :halt])})))
+
+  (is (= (core/massage-rules :my-id [{:when :any-events-seen? :events #{:1 :2} :dispatch (list [:2] :halt)}])
+         (list {:id 0 :when core/any-events-seen? :events #{:1 :2} :dispatch (list [:2] [:my-id :halt])}))))
+
+(deftest test-make-flow-handler-steup
+  (let [flow   {:first-dispatch [:1]
+                :rules [
+                        {:when :seen? :events :1 :dispatch [:2]}
+                        {:when :seen? :events :3 :dispatch :halt}]}
+        handler   (core/make-flow-event-handler flow)]
+    (is (= (handler {:db {}} :setup)
+           {:db {}
+            :dispatch [:1]
+            :event-forwarder {:register core/default-id
+                              :events #{:1 :3}
+                              :dispatch-to [core/default-id]}}))))
+
+
+#_(deftest test-make-flow-handler-forward
+  (let [flow   {:first-dispatch [:1]
+                :id             :test-id
+                :db-path        [:p]
+                :rules [
+                        {:when :seen? :events :1 :dispatch [:2]}
+                        {:when :seen? :events :3 :dispatch :halt}]}
+        handler   (core/make-flow-event-handler flow)]
+    (is (= (handler {:db {:p {:seen-events #{:33} :started-tasks #{}}}} [:1])
+           {:db {}
+            :deregister-event-handler core/default-id
+            :event-forwarder {:unregister core/default-id}}))))
+
+(deftest test-make-flow-handler-halt
+  (let [flow   {:first-dispatch [:1]
+                :rules [
+                        {:when :seen? :events :1 :dispatch [:2]}
+                        {:when :seen? :events :3 :dispatch :halt}]}
+        handler   (core/make-flow-event-handler flow)]
+    (is (= (handler {:db {}} :halt)
+           {:db {}
+            :deregister-event-handler core/default-id
+            :event-forwarder {:unregister core/default-id}}))))
