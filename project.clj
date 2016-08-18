0cce2c474c63bb9bb32d33cddb935adbbce58c01
Mike Thompson <mike.thompson@day8.com.au>
2016-08-18 17:11:05 +1000

    Use the latest alpha of re-frame

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license      {:name "MIT"}
   :dependencies [[org.clojure/clojure        "1.8.0"]
                  [org.clojure/clojurescript  "1.9.89"]
-                 [re-frame                   "0.8.0-alpha9"]
+                 [re-frame                   "0.8.0-alpha12"]
                  [day8.re-frame/forward-events-fx "0.0.4"]]
 
   :profiles {:debug {:debug true}
