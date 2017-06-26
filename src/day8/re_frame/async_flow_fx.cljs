5a433140c6ced23b0bad8ebb7e78be93a53ae362
Stuart Mitchell <github@stuartmitchell.com>
2017-06-26 13:13:11 +1200

    Changed via local copy forward-events-fx

--- src/day8/re_frame/async_flow_fx.cljs
+++ src/day8/re_frame/async_flow_fx.cljs
@@ -2,7 +2,7 @@
   (:require
     [re-frame.core :as re-frame]
     [clojure.set :as set]
-    [day8.re-frame.forward-events-fx]))
+    #_[day8.re-frame.forward-events-fx]))
 
 (defn dissoc-in
   "Dissociates an entry from a nested associative structure returning a new
@@ -37,6 +37,31 @@
                   (ex-info (str (pr-str callback-pred)
                              " isn't an event predicate")
                     {:callback-pred callback-pred})))))
+
+(re-frame/reg-fx
+  :forward-events
+  (let [id->listen-fn     (atom {})
+        process-one-entry (fn [{:as m :keys [unregister register events dispatch-to]}]
+                            (let [_ (assert (map? m) (str "re-frame: effects handler for :forward-events expected a map or a list of maps. Got: " m))
+                                  _ (assert (or (= #{:unregister} (-> m keys set))
+                                              (= #{:register :events :dispatch-to} (-> m keys set))) (str "re-frame: effects handler for :forward-events given wrong map keys" (-> m keys set)))]
+                              (if unregister
+                                (let [f (@id->listen-fn unregister)
+                                      _ (assert (some? f) (str ":forward-events  asked to unregister an unknown id: " unregister))]
+                                  (re-frame/remove-post-event-callback f)
+                                  (swap! id->listen-fn dissoc unregister))
+                                (let [events-preds           (map as-callback-pred events)
+                                      post-event-callback-fn (fn [event-v _]
+                                                               (when (some (fn [pred] (pred event-v))
+                                                                       events-preds)
+                                                                 (re-frame/dispatch (conj dispatch-to event-v))))]
+                                  (re-frame/add-post-event-callback post-event-callback-fn)
+                                  (swap! id->listen-fn assoc register post-event-callback-fn)))))]
+    (fn [val]
+      (cond
+        (map? val)        (process-one-entry val)
+        (sequential? val) (doall (map process-one-entry val))
+        :else (re-frame/console :error  ":forward-events expected a map or a list of maps, but got: " val)))))
 
 (defn seen-all-of?
   [required-events seen-events]
@@ -119,7 +144,6 @@
                       (fn [_] @local-store))
 
         rules       (massage-rules rules)]       ;; all of the events refered to in the rules
-
     ;; Return an event handler which will manage the flow.
     ;; This event handler will receive 3 kinds of events:
     ;;   (dispatch [:id :setup])
