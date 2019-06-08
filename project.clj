04959d524b7f04a8f7090c89012e81a0c1921669
Isaac Johnston <isaac@superstruct.nz>
2019-06-08 12:18:44 +0300

    Fix Clojars deployment credentials

--- project.clj
+++ project.clj
@@ -28,15 +28,17 @@
                                       :linux   "xdg-open"}}}
 
   ;; > lein deploy
-  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
-                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
+  :deploy-repositories [["clojars"  {:sign-releases false
+                                     :url "https://clojars.org/repo"
+                                     :username :env/CLOJARS_USERNAME
+                                     :password :env/CLOJARS_PASSWORD}]]
 
   ;; > lein release
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag" "v" "--no-sign"]
-                  ["deploy"]
+                  ["deploy" "clojars"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
