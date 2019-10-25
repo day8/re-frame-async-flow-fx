50bebc3d0456c61b50e118b07ce7c99b26a837ff
Isaac Johnston <isaac@superstruct.nz>
2019-10-25 18:45:52 +0800

    Migrate to GitHub Actions

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject day8.re-frame/async-flow-fx "0.1.1-SNAPSHOT"
-  :description  "A re-frame effects handler for coordinating the kind of async control flow which often happens on app startup."
-  :url          "https://github.com/day8/re-frame-async-flow-fx.git"
-  :license      {:name "MIT"}
+  :description "A re-frame effects handler for coordinating the kind of async control flow which often happens on app startup."
+  :url "https://github.com/day8/re-frame-async-flow-fx.git"
+  :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/clojurescript "1.10.520" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
@@ -19,16 +19,16 @@
                      :plugins      [[lein-ancient "0.6.15"]
                                     [lein-shell "0.5.0"]]}}
 
-  :clean-targets  [:target-path
-                   "resources/public/js/test"
-                   "shadow-cljs.edn"
-                   "package.json"
-                   "package-lock.json"]
+  :clean-targets [:target-path
+                  "resources/public/js/test"
+                  "shadow-cljs.edn"
+                  "package.json"
+                  "package-lock.json"]
 
   :resource-paths ["run/resources"]
-  :jvm-opts       ["-Xmx1g"]
-  :source-paths   ["src"]
-  :test-paths     ["test"]
+  :jvm-opts ["-Xmx1g"]
+  :source-paths ["src"]
+  :test-paths ["test"]
 
   :shadow-cljs {:builds {:browser-test
                          {:target    :browser-test
@@ -42,15 +42,15 @@
                           :ns-regexp "-test$"
                           :output-to "target/karma-test.js"}}}
 
-  :shell          {:commands {"open" {:windows ["cmd" "/c" "start"]
-                                      :macosx  "open"
-                                      :linux   "xdg-open"}}}
+  :shell {:commands {"open" {:windows ["cmd" "/c" "start"]
+                             :macosx  "open"
+                             :linux   "xdg-open"}}}
 
   ;; > lein deploy
-  :deploy-repositories [["clojars"  {:sign-releases false
-                                     :url "https://clojars.org/repo"
-                                     :username :env/CLOJARS_USERNAME
-                                     :password :env/CLOJARS_PASSWORD}]]
+  :deploy-repositories [["clojars" {:sign-releases false
+                                    :url           "https://clojars.org/repo"
+                                    :username      :env/CLOJARS_USERNAME
+                                    :password      :env/CLOJARS_PASSWORD}]]
 
   ;; > lein release
   :release-tasks [["vcs" "assert-committed"]
@@ -62,7 +62,10 @@
                   ["vcs" "commit"]
                   ["vcs" "push"]]
 
-  :aliases {"dev-auto" ["shadow" "watch" "browser-test"]
-            "test-once" ["do"
-                         ["shadow" "compile" "karma-test"]
-                         ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]})
+  :aliases {"dev-auto"   ["do"
+                          ["clean"]
+                          ["shadow" "watch" "browser-test"]]
+            "karma-once" ["do"
+                          ["clean"]
+                          ["shadow" "compile" "karma-test"]
+                          ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]})
