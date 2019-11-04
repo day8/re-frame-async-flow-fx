2d866693e97da2805544d4b5949e95f054aeafad
Isaac Johnston <isaac@superstruct.nz>
2019-11-04 10:19:45 +0800

    Reformat project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject day8.re-frame/async-flow-fx "see :git-version below https://github.com/arrdem/lein-git-version"
+(defproject    day8.re-frame/async-flow-fx "see :git-version below https://github.com/arrdem/lein-git-version"
   :description "A re-frame effects handler for coordinating the kind of async control flow which often happens on app startup."
-  :url "https://github.com/day8/re-frame-async-flow-fx.git"
-  :license {:name "MIT"}
+  :url         "https://github.com/day8/re-frame-async-flow-fx.git"
+  :license     {:name "MIT"}
 
   :git-version
   {:status-to-version
@@ -15,23 +15,23 @@
              patch+           (inc patch)]
          (format "%s.%d-%s-SNAPSHOT" prefix patch+ ahead))))}
 
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.520" :scope "provided"
+  :dependencies [[org.clojure/clojure             "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript       "1.10.520" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [thheller/shadow-cljs "2.8.69" :scope "provided"]
-                 [re-frame "0.10.9" :scope "provided"]
+                 [thheller/shadow-cljs            "2.8.69" :scope "provided"]
+                 [re-frame                        "0.10.9" :scope "provided"]
                  [day8.re-frame/forward-events-fx "0.0.6"]]
 
   :plugins [[me.arrdem/lein-git-version "2.0.3"]
-            [lein-shadow "0.1.5"]]
+            [lein-shadow                "0.1.5"]]
 
   :profiles {:debug {:debug true}
-             :dev   {:dependencies [[karma-reporter "3.1.0"]
+             :dev   {:dependencies [[karma-reporter     "3.1.0"]
                                     [day8.re-frame/test "0.1.5"]
                                     [binaryage/devtools "0.9.10"]]
                      :plugins      [[lein-ancient "0.6.15"]
-                                    [lein-shell "0.5.0"]]}}
+                                    [lein-shell   "0.5.0"]]}}
 
   :clean-targets [:target-path
                   "resources/public/js/test"
