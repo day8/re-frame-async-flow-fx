bd816f99f6230950d3c955690902ed0f5da2b047
Isaac Johnston <isaac@superstruct.nz>
2020-09-28 15:27:25 +1300

    Upgrade lein-shadow to 0.3.1

--- project.clj
+++ project.clj
@@ -10,29 +10,26 @@
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs            "2.11.0" :scope "provided"]
+                 [thheller/shadow-cljs            "2.11.4" :scope "provided"]
                  [re-frame                        "1.1.1" :scope "provided"]
                  [day8.re-frame/forward-events-fx "0.0.6"]]
 
   :plugins      [[day8/lein-git-inject "0.0.14"]
-                 [lein-shadow          "0.2.2"]
+                 [lein-shadow          "0.3.1"]
                  [lein-ancient         "0.6.15"]
-                 [lein-pprint          "1.3.2"]]
+                 [lein-pprint          "1.3.2"]
+                 [lein-shell           "0.5.0"]]
 
   :middleware   [leiningen.git-inject/middleware]
 
   :profiles {:debug {:debug true}
-             :dev   {:dependencies [[karma-reporter     "3.1.0"]
-                                    [day8.re-frame/test "0.1.5"]
-                                    [binaryage/devtools "1.0.2"]]
-                     :plugins      [[lein-ancient "0.6.15"]
-                                    [lein-shell   "0.5.0"]]}}
+             :dev   {:dependencies [[day8.re-frame/test "0.1.5"]
+                                    [binaryage/devtools "1.0.2"]]}}
 
   :clean-targets [:target-path
                   "resources/public/js/test"
                   "shadow-cljs.edn"
-                  "package.json"
-                  "package-lock.json"]
+                  "node_modules"]
 
   :resource-paths ["run/resources"]
   :jvm-opts ["-Xmx1g"]
