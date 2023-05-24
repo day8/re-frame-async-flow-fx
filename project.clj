95580442658a87e773711f3ba61c702161d8cc2b
hipitihop <anonymous.amphibian@gmail.com>
2023-05-24 12:17:51 +1000

    Bump provided dependencies including clojure 1.11.1 and clojurescript 1.11.60

--- project.clj
+++ project.clj
@@ -5,26 +5,28 @@
 
   :min-lein-version "2.9.0"
 
-  :dependencies [[org.clojure/clojure             "1.10.3" :scope "provided"]
-                 [org.clojure/clojurescript       "1.10.844" :scope "provided"
+  :dependencies [[org.clojure/clojure             "1.11.1" :scope "provided"]
+                 [org.clojure/clojurescript       "1.11.60" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs            "2.12.4" :scope "provided"]
-                 [re-frame                        "1.2.0" :scope "provided"]
+                 [thheller/shadow-cljs            "2.23.3" :scope "provided"]
+                 [re-frame                        "1.3.0" :scope "provided"]
                  [day8.re-frame/forward-events-fx "0.0.6"]]
 
-  :plugins      [[day8/lein-git-inject "0.0.14"]
-                 [lein-shadow          "0.3.1"]
-                 [lein-ancient         "0.6.15"]
-                 [lein-pprint          "1.3.2"]
-                 [lein-shell           "0.5.0"]]
+  :plugins      [[com.github.liquidz/antq "RELEASE"]
+                 [day8/lein-git-inject "0.0.15"]
+                 [lein-pprint "1.3.2"]
+                 [lein-shadow "0.4.1"]
+                 [lein-shell  "0.5.0"]]
 
   :middleware   [leiningen.git-inject/middleware]
 
+  :antq         {}
+
   :profiles {:debug {:debug true}
              :dev   {:dependencies [[day8.re-frame/test "0.1.5"]
-                                    [binaryage/devtools "1.0.3"]]}}
+                                    [binaryage/devtools "1.0.7"]]}}
 
   :clean-targets [:target-path
                   "resources/public/js/test"
@@ -57,6 +59,9 @@
 
   :shell {:commands {"karma" {:windows         ["cmd" "/c" "karma"]
                               :default-command "karma"}
+                     "npm"   {:exit-code       :ignore
+                              :windows         ["cmd" "/c" "npm"]
+                              :default-command "npm"}
                      "open"  {:windows         ["cmd" "/c" "start"]
                               :macosx          "open"
                               :linux           "xdg-open"}}}
@@ -64,6 +69,12 @@
   :aliases {"watch" ["do"
                      ["clean"]
                      ["shadow" "watch" "browser-test" "karma-test"]]
+            "outdated" ["with-profile" "dev,test" "do"
+                        ["shell" "echo" "Upgrade the following dependencies with 'npm install name@ver --save':"]
+                        ["shell" "npm" "outdated"]
+                        ["shell" "echo" "\n\n"]
+                        ["shell" "echo" "Upgrade the following dependencies by editing project.clj:"]
+                        ["antq"]]
 
             "ci"    ["do"
                      ["clean"]
--- src/deps.cljs
+++ src/deps.cljs
@@ -1,5 +1,5 @@
-{:npm-dev-deps {"shadow-cljs"           "2.12.4"
-                "karma"                 "6.3.2"
-                "karma-chrome-launcher" "3.1.0"
+{:npm-dev-deps {"shadow-cljs"           "2.23.3"
+                "karma"                 "6.4.2"
+                "karma-chrome-launcher" "3.2.0"
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"}}
